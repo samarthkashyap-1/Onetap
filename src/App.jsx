212eabe6a4a6030067144144166ba342f0c0fb5e
samarth kashyap <samarthkashyap318@gmail.com>
2023-10-14 17:14:34 +0530

    landing page updated

--- src/App.jsx
+++ src/App.jsx
@@ -32,46 +32,55 @@ function App() {
 
   return (
     <>
-      <img src={wave} alt="" className="absolute top-0 left-0 right-0 -z-10 " />
-      <Navbar />
-      <div className="h-screen px-20 flex ">
-        <div className=" mt-40 text-primary flex-1">
-          <h1 className="text-3xl mt-10 text-white">Hola!!!</h1>
-          <h1 className="text-3xl mt-6">
-            Welcome to <p className="text-white text-7xl">One Tap</p>{" "}
-          </h1>
-          <p className="text-primary text-2xl mt-4">
-            Streamline, Simplify, One Tap!
+      <h1 className="hidden sm:block md:block text-5xl text-center mt-96">
+        Currently not supported on mobile and tablet devices
+      </h1>
+      <div className="sm:hidden md:hidden">
+        <img
+          src={wave}
+          alt=""
+          className="absolute top-0 left-0 right-0 -z-10 "
+        />
+        <Navbar />
+        <div className="h-screen px-20 flex ">
+          <div className=" mt-40 text-primary flex-1">
+            <h1 className="text-3xl mt-10 text-white">Hola!!!</h1>
+            <h1 className="text-3xl mt-6">
+              Welcome to <p className="text-white text-7xl">One Tap</p>{" "}
+            </h1>
+            <p className="text-primary text-2xl mt-4">
+              Streamline, Simplify, One Tap!
+            </p>
+            <h1 className="text-4xl mt-12">
+              Where Streamlining and Simplifying are Just One Tap Away!{" "}
+            </h1>
+          </div>
+
+          <div className=" flex-1 mt-40 ">
+            <img
+              src={blob}
+              alt=""
+              className=" scale-75 mt-10 -z-10 absolute top-0"
+            />
+            <img src={demo} alt="" className=" scale-90 mt-10" />
+          </div>
+        </div>
+        <div className=" px-20 ">
+          <h1 className="text-4xl text-primary">About Us</h1>
+          <p className="text-sec text-xl mt-12">
+            We're dedicated to making your life simpler and more streamlined.
+            With "One Tap," you can access all the essential information and
+            resources you need in just a single tap. Our commitment is to
+            provide convenience at your fingertips.
           </p>
-          <h1 className="text-4xl mt-12">
-            Where Streamlining and Simplifying are Just One Tap Away!{" "}
-          </h1>
         </div>
 
-        <div className=" flex-1 mt-40 ">
-          <img
-            src={blob}
-            alt=""
-            className=" scale-75 mt-10 -z-10 absolute top-0"
-          />
-          <img src={demo} alt="" className=" scale-90 mt-10" />
-        </div>
-      </div>
-      <div className=" px-20 ">
-        <h1 className="text-4xl text-primary">About Us</h1>
-        <p className="text-sec text-xl mt-12">
-          We're dedicated to making your life simpler and more streamlined. With
-          "One Tap," you can access all the essential information and resources
-          you need in just a single tap. Our commitment is to provide
-          convenience at your fingertips.
-        </p>
-      </div>
-
-      <div className="relative mt-20">
-        <img src={wave2} alt="" className="w-full" />
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
-          <p>Design and Developed by</p>
-          <p>Samarth Kashyap</p>
+        <div className="relative mt-20">
+          <img src={wave2} alt="" className="w-full" />
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+            <p>Design and Developed by</p>
+            <p>Samarth Kashyap</p>
+          </div>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
     extend: {
       colors: {
         primary: "#011C37",
